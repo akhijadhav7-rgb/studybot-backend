83b6df35dcdf007ed8b8cf407406d244148bd196
akhijadhav7-rgb <akhijadhav7@gmail.com>
2026-02-07 17:00:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from openai import OpenAI
+import os
 
+app = FastAPI()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+@app.get("/")
+def home():
+    return {"message": "StudyBot is running"}
+
+@app.post("/chat")
+def chat(q: str):
+    r = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": q}]
+    )
+    return {"answer": r.choices[0].message.content}
